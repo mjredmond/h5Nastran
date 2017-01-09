5f03874da0c1df142059a788f7df52fbe9c2fe4d
Michael Redmond <michael.j.redmond@gmail.com>
2017-01-09 16:51:30 +0000

    edit shear

--- mrPunch/_table_formats/elemental/force/shear.py
+++ mrPunch/_table_formats/elemental/force/shear.py
@@ -1,4 +1,7 @@
 """
+Copyright (C) Michael James Redmond, Jr - All Rights Reserved
+Unauthorized copying of this file, via any medium is strictly prohibited
+Proprietary and confidential
 Written by Michael James Redmond, Jr.
 """
 from __future__ import print_function, absolute_import
